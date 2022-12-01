3fbfdf3c21c22f5f605016980fc222e07be4b2e4
Juan Manuel Perez Toro <juanma97@outlook.com>
2022-12-01 16:03:22 +0000

    Update readme

--- src/medium/02.ts
+++ src/medium/02.ts
@@ -20,7 +20,7 @@
  * Example 3:
  * 
  * Input: "pwwkew"
- * Ouput: 3
+ * Output: 3
  * Explanation: The answer is "wke", with the length of 3.
  * 
  * Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
